e4f904af4f21bc4888c96d214f5e3e3e55e5b9df
Almavelez <74258817+Almavelez@users.noreply.github.com>
2021-04-02 17:43:46 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDlPD5mqOXh18fjTIn7Z95dB3JHiKXqf6Y",
+  authDomain: "controlavmz.firebaseapp.com",
+   projectId: "controlavmz",
+  storageBucket: "controlavmz.appspot.com",
+  messagingSenderId: "340353702412",
+  appId: "1:340353702412:web:3e91d4140bdf5ca40dac29",
+  measurementId: "G-E7GBEMTN4Z"
+});
+  
